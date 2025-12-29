462f657453b15e780ecb3116b1e2b7d86e2e446f
Dheeraj jha <dheeraj@thedijje.com>
2025-12-29 18:07:23 +0530

    Updated meta details

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,21 +6,21 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-    title: 'Dheeraj Jha | Senior DevOps Engineer & Software Architect',
-    description: 'Portfolio of Dheeraj Jha, a Senior DevOps Engineer and Software Architect specializing in AWS, CI/CD, Docker, and Scalable Backend Systems.',
+    title: 'Dheeraj Jha | ☁️ AWS + DevOps | 🌿 Explorer & Learner',
+    description: 'Portfolio of Dheeraj Jha, a DevOps Engineer focused on cloud infrastructure, automation, and scalable systems—driven by curiosity, clarity, and real-world problem solving.',
     keywords: 'Dheeraj Jha, DevOps Engineer, Software Architect, AWS, CI/CD, Docker, Kubernetes, PHP, Python, Full Stack Developer, Faridabad',
     authors: [{ name: 'Dheeraj Jha' }],
     openGraph: {
         type: 'website',
         url: 'https://itsdheeraj.me/',
-        title: 'Dheeraj Jha | Senior DevOps Engineer & Software Architect',
-        description: 'Senior DevOps Engineer & Software Architect with 9+ years of experience building and scaling, high-performance systems.',
+        title: 'Dheeraj Jha | ☁️ AWS + DevOps | 🌿 Explorer & Learner',
+        description: 'DevOps Engineer focused on cloud infrastructure, automation, and scalable systems—driven by curiosity, clarity, and real-world problem solving.',
         images: ['./assets/hero-meta.jpg'],
     },
     twitter: {
         card: 'summary_large_image',
-        title: 'Dheeraj Jha | Senior DevOps Engineer & Software Architect',
-        description: 'Senior DevOps Engineer & Software Architect with 9+ years of experience building and scaling, high-performance systems.',
+        title: 'Dheeraj Jha | ☁️ AWS + DevOps | 🌿 Explorer & Learner',
+        description: 'DevOps Engineer focused on cloud infrastructure, automation, and scalable systems—driven by curiosity, clarity, and real-world problem solving.',
         images: ['./assets/hero-meta.jpg'],
     },
 }
